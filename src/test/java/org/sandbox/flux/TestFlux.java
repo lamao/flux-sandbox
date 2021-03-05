b547076ab3bb2bb04d1870e4985ee496d674d9ca
vyacheslav.mischeryakov <vyacheslav.mischeryakov@pragmaticplay.com>
2021-03-05 12:52:09 +0200

    Initial commit

--- src/test/java/org/sandbox/flux/TestFlux.java
+++ src/test/java/org/sandbox/flux/TestFlux.java
@@ -4,11 +4,8 @@ import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.LongAccumulator;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Created by vyacheslav.mischeryakov
